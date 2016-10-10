66dc71d20c20ccd447eedba47e5a5551e8747bbc
Wes Davis <wesley.r.davis@gmail.com>
2016-10-09 21:37:09 -0400

    Cisco Controller device modeler cleanup

--- ZenPacks/daviswr/WirelessController/modeler/plugins/daviswr/snmp/CiscoController.py
+++ ZenPacks/daviswr/WirelessController/modeler/plugins/daviswr/snmp/CiscoController.py
@@ -14,14 +14,24 @@ class CiscoController(SnmpPlugin):
     maptype = 'CiscoController'
 
     snmpGetMap = GetMap({
-        '.1.3.6.1.4.1.14179.1.1.1.3.0': 'agentInventoryMachineModel',
-        '.1.3.6.1.4.1.14179.1.1.1.4.0': 'agentInventorySerialNumber',
-        '.1.3.6.1.4.1.14179.1.1.1.12.0': 'agentInventoryManufacturerName',
-        '.1.3.6.1.4.1.14179.1.1.1.14.0': 'agentInventoryProductVersion',
-        '.1.3.6.1.4.1.14179.1.1.5.2.0': 'agentTotalMemory',
-        '.1.3.6.1.4.1.14179.2.3.1.17.0': 'bsnRFMobilityDomainName',
-        '.1.3.6.1.4.1.14179.1.1.1.18.0': 'agentInventoryMaxNumberOfAPsSupported',
-        '.1.3.6.1.2.1.47.1.1.1.1.8.1': 'entPhysicalHardwareRev',
+        # entPhysicalDescr
+        '.1.3.6.1.2.1.47.1.1.1.1.2.1': 'snmpDescr',
+        # entPhysicalHardwareRev
+        '.1.3.6.1.2.1.47.1.1.1.1.8.1': 'hwVersion',
+        # agentInventoryMachineModel
+        '.1.3.6.1.4.1.14179.1.1.1.3.0': 'model',
+        # agentInventorySerialNumber
+        '.1.3.6.1.4.1.14179.1.1.1.4.0': 'setHWSerialNumber',
+        # agentInventoryManufacturerName
+        '.1.3.6.1.4.1.14179.1.1.1.12.0': 'manufacturer',
+        # agentInventoryProductVersion
+        '.1.3.6.1.4.1.14179.1.1.1.14.0': 'version',
+        # agentInventoryMaxNumberOfAPsSupported
+        '.1.3.6.1.4.1.14179.1.1.1.18.0': 'platformMaxAPs',
+        # agentTotalMemory
+        '.1.3.6.1.4.1.14179.1.1.5.2.0': 'memory',
+        # bsnRFMobilityDomainName
+        '.1.3.6.1.4.1.14179.2.3.1.17.0': 'mobility',
         })
 
     def process(self, device, results, log):
@@ -39,82 +49,36 @@ class CiscoController(SnmpPlugin):
                 )
             return None
 
-        om = self.objectMap()
         maps = list()
 
-        manufacturer = getdata.get('agentInventoryManufacturerName', '')
+        # Hardware model
+        manufacturer = getdata.get('manufacturer', '')
         if manufacturer.lower().find('cisco') > -1 or len(manufacturer) == 0:
             manufacturer = 'Cisco'
 
-        model = getdata.get('agentInventoryMachineModel', None)
-        if model:
-            log.debug(
-                '%s setting model to %s %s',
-                self.name(),
-                manufacturer,
-                model
-                )
-            om.setHWProductKey = MultiArgs(model, manufacturer)
+        if 'model' in getdata:
+            getdata['setHWProductKey'] = MultiArgs(getdata['model'], manufacturer)
 
+        # Software version
         os = 'AireOS'
-        version = getdata.get('agentInventoryProductVersion', None)
-        if version:
-            os = '{0} {1}'.format(os, version)
-        log.debug(
-            '%s setting model to %s %s',
-            self.name(),
-            manufacturer,
-            os
-            )
-        om.setOSProductKey = MultiArgs(os, manufacturer)
+        if 'version' in getdata:
+            os = '{0} {1}'.format(os, getdata['version'])
+        getdata['setOSProductKey'] = MultiArgs(os, manufacturer)
 
-        maps.append(om)
+        # Mobility Domain
+        if 'mobility' in getdata:
+            getdata.update({'mobilityDomains': [getdata['mobility'],]})
 
-        serial = getdata.get('agentInventorySerialNumber', None)
-        if serial:
-            log.debug(
-                '%s setting serial number to %s',
-                self.name(),
-                serial
-                )
-            maps.append(ObjectMap({'setHWSerialNumber': serial}))
+        maps.append(ObjectMap(
+            modname='ZenPacks.daviswr.WirelessController.CiscoController',
+            data=getdata
+            ))
 
-        memory = getdata.get('agentTotalMemory', None)
-        if memory:
+        # Memory
+        if 'memory' in getdata:
             # AIRESPACE-SWITCHING-MIB::agentTotalMemory is in kilobytes
-            memory = str(int(memory)*1024)
-            log.debug(
-                '%s setting memory size to %s bytes',
-                self.name(),
-                memory
-                )
+            memory = int(getdata['memory'])*1024
             maps.append(ObjectMap({'totalMemory': memory}, compname='hw'))
 
-        mobility = getdata.get('bsnRFMobilityDomainName', None)
-        if mobility:
-            log.debug(
-                '%s setting mobility domain to %s',
-                self.name(),
-                mobility
-                )
-            maps.append(ObjectMap({'mobilityDomains': [mobility,]}))
-
-        max_ap = getdata.get('agentInventoryMaxNumberOfAPsSupported', None)
-        if max_ap:
-            log.debug(
-                '%s setting platform max APs to %s',
-                self.name(),
-                max_ap
-                )
-            maps.append(ObjectMap({'platformMaxAPs': int(max_ap)}))
-
-        hw_ver = getdata.get('entPhysicalHardwareRev', None)
-        if hw_ver:
-            log.debug(
-                '%s setting hardware version to %s',
-                self.name(),
-                hw_ver
-                )
-            maps.append(ObjectMap({'hwVersion': hw_ver}))
-
+        log.debug('%s ObjMaps:\n%s', self.name(), str(maps))
         return maps
