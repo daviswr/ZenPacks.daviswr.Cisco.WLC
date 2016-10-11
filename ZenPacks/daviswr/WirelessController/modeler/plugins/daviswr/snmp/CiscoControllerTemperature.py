2aad8cfe803a7171064f8aff8e35cf9797149e16
Wes Davis <wesley.r.davis@gmail.com>
2016-10-10 23:48:37 -0400

    Cisco temperature sensor modeler

--- ZenPacks/daviswr/WirelessController/modeler/plugins/daviswr/snmp/CiscoControllerTemperature.py
+++ ZenPacks/daviswr/WirelessController/modeler/plugins/daviswr/snmp/CiscoControllerTemperature.py
@@ -16,6 +16,21 @@ class CiscoControllerTemperature(SnmpPlugin):
     relname = 'temperaturesensors'
     modname = 'Products.ZenModel.TemperatureSensor'
 
+    bsnGlobalDot11Config = {
+        # bsnOperatingTemperatureEnvironment
+        '.12': 'state',
+        # bsnSensorTemperature
+        '.13': 'temperature_celsius'
+        }
+
+    snmpGetTableMaps = (
+        GetTableMap(
+            'bsnSensorTemperature',
+            '.1.3.6.1.4.1.14179.2.3.1',
+            bsnGlobalDot11Config
+            ),
+        )
+
     def process(self, device, results, log):
         """collect snmp information from this device"""
         log.info('processing %s for device %s', self.name(), device.id)
@@ -24,4 +39,44 @@ class CiscoControllerTemperature(SnmpPlugin):
 
         log.debug('SNMP Tables:\n%s', tabledata)
 
-        pass
+        bsnSensorTemperature = tabledata.get('bsnSensorTemperature')
+        if bsnSensorTemperature is None:
+            log.error('Unable to get bsnSensorTemperature for %s', device.id)
+            return None
+        else:
+            log.debug(
+                'bsnSensorTemperature has %s entries',
+                str(len(bsnSensorTemperature)),
+                )
+
+        # Temperator sensors
+        rm = self.relMap()
+
+        for snmpindex in bsnSensorTemperature:
+            row = bsnSensorTemperature[snmpindex]
+            name = 'Temperature Sensor {}'.format(snmpindex.replace('.', ''))
+
+            # Clean up attributes
+            attr_map = dict()
+            attr_map['state'] = {
+                1: 'Commercial',
+                2: 'Industrial',
+                }
+
+            for attr in attr_map:
+                if attr in row:
+                    row[attr] = attr_map[attr].get(row[attr], 'Unknown')
+
+            if 'temperature_celsius' in row:
+                temp_f = (row['temperature_celsius'] * (9/5)) + 32
+                row['temperature_fahrenheit'] = temp_f
+
+            row.update({
+                'id': self.prepId(name.replace(' ', '')),
+                'title': name,
+                'snmpindex': snmpindex.strip('.'),
+                })
+            rm.append(self.objectMap(row))
+
+        log.debug('%s RelMap:\n%s', self.name(), str(rm))
+        return rm
